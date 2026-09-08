65d40947f888b0844d4372d363d2542289064cd9
Maria Paula Silva <smariapaula0625@gmail.com>
2026-09-08 16:34:45 -0300

    Começando a estudar ES6+

---
+++ Aulas/JavaScript/objetos/script.js
@@ -0,0 +1,13 @@
+// Objetos
+
+let pessoa = {
+    nome: "João",
+    idade: 25,
+    cargo: "Suporte Help Desk"
+};
+
+let usuarios = [
+    {nome: "Maria", idade: 20, cargo:"Programadora Fullstack"},
+    {nome: "Rita", idade: 30, cargo:"Gestora de marketing"},
+    {nome: "Paulo", idade: 45, cargo:"CEO"}
+];
---
+++ Aulas/JavaScript/tamplateStrings/script.js
@@ -0,0 +1,5 @@
+let nome = "Maria Paula";
+
+let mensagem = `Me chamo ${nome}.`;
+
+console.log(mensagem);
