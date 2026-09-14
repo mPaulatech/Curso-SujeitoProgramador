711f5693b8c6c70bd6413972a8b8f589b91b9cd5
Maria Paula Silva <smariapaula0625@gmail.com>
2026-09-14 16:19:13 -0300

    Estudando ES6+

--- Aulas/JavaScript/ES6+/operaçõesArrays/script.js
+++ Aulas/JavaScript/ES6+/operaçõesArrays/script.js
@@ -4,15 +4,35 @@
 //     console.log(`${item} e ${index}`);
 // });
 
-let numeros = [1, 2, 3];
+// let numeros = [1, 2, 3];
 
-let total = numeros.reduce((acumulador, numero, indice, original)=>{
-    console.log(`${acumulador} - total até o momento`);
-    console.log(`${numero} - numero atual`);
-    console.log(`${indice} - posição do valor`);
-    console.log(`${original} - valor original do array`);
+// let total = numeros.reduce((acumulador, numero, indice, original)=>{
+//     console.log(`${acumulador} - total até o momento`);
+//     console.log(`${numero} - numero atual`);
+//     console.log(`${indice} - posição do valor`);
+//     console.log(`${original} - valor original do array`);
 
-    return acumulador += numero;
-});
+//     return acumulador += numero;
+// });
 
-console.log(`O total é ${total}`);
+// console.log(`O total é ${total}`);
+
+// FIND
+
+// let listagem = [1, "Maria", 4, "João", 2, "Maria"];
+
+// let resultado = listagem.find((item)=>{
+//     return item === "Maria"
+// });
+
+// console.log(resultado);
+
+// FILTER
+
+// let nomes = ["Maria", "João", "Fernanda", "Jose"];
+
+// let buscar = nomes.filter((item)=>{
+//     return item.length <= 4
+// });
+
+// console.log(buscar)
