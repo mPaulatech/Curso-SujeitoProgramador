9522dc143f87965034c9c1b49ea4a4759e749ba3
Maria Paula Silva <smariapaula0625@gmail.com>
2026-09-15 15:38:45 -0300

    Entendo para que serve o includes, startsWith e endsWith

---
+++ Aulas/JavaScript/ES6+/facilitadores/script.js
@@ -0,0 +1,14 @@
+// includes
+let lista = ["Maria", "João"];
+
+console.log(lista.includes("João"));
+
+// startsWith
+let nome = "Maria";
+
+console.log(nome.startsWith("Mar"));
+
+// endsWith
+let pessoas = "Maria";
+
+console.log(pessoas.endsWith("ia"));
