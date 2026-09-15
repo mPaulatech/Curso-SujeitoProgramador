fa0f755412a63e548011add0ccb5639748eca4b2
Maria Paula Silva <smariapaula0625@gmail.com>
2026-09-15 15:23:54 -0300

    Estudei para que ser e como utilizar as funções anônimas

---
+++ Aulas/JavaScript/ES6+/funçõesAnônimas/script.js
@@ -0,0 +1,19 @@
+function somar(a, b){
+    let total = a + b;
+    console.log(total)
+}
+
+somar(25, 30);
+
+let subtrair = (c, d)=>{
+    let total = c - d;
+    console.log(total)
+}
+
+subtrair(50, 5);
+
+let lista = [1, 4, 8, 10];
+
+lista.map((item)=>{
+    console.log(item);
+});
